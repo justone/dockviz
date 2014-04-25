0240a3fa3d421726fad516d48481d31c9ed91c37
Nate Jones <nate@endot.org>
2014-04-25 07:45:08 -0700

    more complete test for graphviz output

--- images_test.go
+++ images_test.go
@@ -24,6 +24,14 @@ func Test_Dot(t *testing.T) {
 				`base -> "4c1208b690c6"`,
 			},
 		},
+		RunTest{
+			json: `[{ "VirtualSize": 662553464, "Size": 0, "RepoTags": [ "foo:latest" ], "ParentId": "735f5db5626147582d2ae3f2c87be8e5e697c088574c5faaf8d4d1bccab99470", "Id": "c87be8e5e697c735f5db5626147582d2ae3f2088574c5faaf8d4d1bccab99470", "Created": 1386142123 },{ "VirtualSize": 662553464, "Size": 0, "RepoTags": [ "<none>:<none>" ], "ParentId": "4c1208b690c68af3476b437e7bc2bcc460f062bda2094d2d8f21a7e70368d358", "Id": "735f5db5626147582d2ae3f2c87be8e5e697c088574c5faaf8d4d1bccab99470", "Created": 1386142123 },{ "VirtualSize": 662553464, "Size": 662553464, "RepoTags": [ "<none>:<none>" ], "ParentId": "", "Id": "4c1208b690c68af3476b437e7bc2bcc460f062bda2094d2d8f21a7e70368d358", "Created": 1386114144 }]`,
+			regexps: []string{
+				`base -> "4c1208b690c6"`,
+				`"4c1208b690c6" -> "735f5db56261"`,
+				`"c87be8e5e697" \[label="c87be8e5e697\\nfoo:latest"`,
+			},
+		},
 	}
 
 	for _, dotTest := range dotTests {
