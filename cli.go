9863e97953bde4fc770c6f1a499513b01f907902
Nate Jones <nate@endot.org>
2016-06-14 12:21:53 -0700

    version bump to v0.4.2

--- cli.go
+++ cli.go
@@ -19,7 +19,7 @@ type GlobalOptions struct {
 var globalOptions GlobalOptions
 var parser = flags.NewParser(&globalOptions, flags.Default)
 
-var version = "v0.4.1"
+var version = "v0.4.2"
 
 func main() {
 	globalOptions.Version = func() {
